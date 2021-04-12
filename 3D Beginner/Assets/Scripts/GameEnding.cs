b737868190f724be14bb69c733c1eabdfd9fcb8e
Tyler Christenson <tchrist7@uoregon.edu>
2021-04-11 21:41:51 -0700

    Enemies Part 2 Added
    Finished up part 1 and added part 2

--- 3D Beginner/Assets/Scripts/GameEnding.cs
+++ 3D Beginner/Assets/Scripts/GameEnding.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameEnding : MonoBehaviour {
+public class GameEnding : MonoBehaviour
+{
     public float fadeDuration = 1f;
     public float displayImageDuration = 1f;
     public GameObject player;
@@ -16,27 +17,34 @@ public class GameEnding : MonoBehaviour {
     bool m_IsPlayerCaught;
     float m_Timer;
 
-    void OnTriggerEnter(Collider other) {
-        if (other.gameObject == player) {
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
             m_IsPlayerAtExit = true;
         }
     }
 
-    public void CaughtPlayer() {
+    public void CaughtPlayer()
+    {
         m_IsPlayerCaught = true;
     }
 
-    void Update() {
-    
-         if (m_IsPlayerAtExit) {
-             EndLevel(exitBackgroundImageCanvasGroup, false, exitAudio);
-         }
-         else if (m_IsPlayerCaught) {
-             EndLevel(caughtBackgroundImageCanvasGroup, true, caughtAudio);
-         }
-     }
+    void Update()
+    {
 
-    void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart, AudioSource audioSource) {
+        if (m_IsPlayerAtExit)
+        {
+            EndLevel(exitBackgroundImageCanvasGroup, false, exitAudio);
+        }
+        else if (m_IsPlayerCaught)
+        {
+            EndLevel(caughtBackgroundImageCanvasGroup, true, caughtAudio);
+        }
+    }
+
+    void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart, AudioSource audioSource)
+    {
         m_Timer += Time.deltaTime;
         imageCanvasGroup.alpha = m_Timer / fadeDuration;
 
@@ -47,13 +55,16 @@ public class GameEnding : MonoBehaviour {
 
         }
 
-        if (m_Timer > fadeDuration + displayImageDuration) {
-            if (doRestart) {
+        if (m_Timer > fadeDuration + displayImageDuration)
+        {
+            if (doRestart)
+            {
                 SceneManager.LoadScene(0);
-            } else {
+            }
+            else
+            {
                 Application.Quit();
             }
         }
-
     }
 }
